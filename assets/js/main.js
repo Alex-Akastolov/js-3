bcb64736e9133da4773fcec4ec684cdf731599a0
Oleksii Akastolov <akastolov1@gmail.com>
2022-06-18 20:43:49 +0300

    js 3

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,5 +1,9 @@
 console.log('Sample JavaScript #3 HW #15');
 
+
+console.log('---------------')
+
+
 /*
  * #1
  *
@@ -9,21 +13,34 @@ console.log('Sample JavaScript #3 HW #15');
  * если нет – то счет продолжается
  */
 
-// console.log(counter()); // 0
 
-// console.log(counter()); // 1
+let counter = (function() {
+    let count = 0;
 
-// console.log(counter(100)); // 100
+    return function (num) {
+        count = num === undefined ? count : num;
 
-// console.log(counter()); // 101
+        return count++;
+    };
+}())
 
-// console.log(counter(500)); // 500
+console.log(counter()); // 0
 
-// console.log(counter()); // 501
+console.log(counter()); // 1
 
-// console.log(counter(0)); // 0
+console.log(counter(100)); // 100
 
-// console.log(counter()); // 1
+console.log(counter()); // 101
+
+console.log(counter(500)); // 500
+
+console.log(counter()); // 501
+
+console.log(counter(0)); // 0
+
+console.log(counter()); // 1
+
+console.log('---------------')
 
 /*
  * #2
@@ -36,33 +53,55 @@ console.log('Sample JavaScript #3 HW #15');
  * counting.decrement() – уменьшает значение счетчика на 1
  */
 
-// console.log(counting.value()); // 0
 
-// counting.increment();
+let counting = (function (){
+    let count = 0;
+    return {
+        value(num) {
+            if (num !== undefined) count = num;
+            return count;
+        },
+        decrement() {
+            count--;
+        },
+        increment() {
+            count++;
+        }
+    };
+}());
 
-// counting.increment();
+console.log(counting.value()); // 0
 
-// counting.increment();
+counting.increment();
 
-// console.log(counting.value()); // 3
+counting.increment();
 
-// counting.decrement();
+counting.increment();
 
-// counting.decrement();
+console.log(counting.value()); // 3
 
-// console.log(counting.value()); // 1
+counting.decrement();
 
-// console.log(counting.value(100)); // 100
+counting.decrement();
 
-// counting.decrement();
+console.log(counting.value()); // 1
 
-// console.log(counting.value()); // 99
+console.log(counting.value(100)); // 100
 
-// console.log(counting.value(200)); // 200
+counting.decrement();
 
-// counting.increment();
+console.log(counting.value()); // 99
 
-// console.log(counting.value()); // 201
+console.log(counting.value(200)); // 200
+
+counting.increment();
+
+console.log(counting.value()); // 201
+
+
+
+console.log('---------------')
+
 
 /*
  * #3
@@ -76,57 +115,107 @@ console.log('Sample JavaScript #3 HW #15');
  * console.log(myPow(2, 3, myPrint)); // 2^3=8
  */
 
-//  console.log(myPow(3, 4, myPrint)); // 3^4=81
+let myPrint = (a, b, res) => `${a}^${b}=${res}`;
+let myPow = (a, b, callback) => {
+    let pow = (x, n) => {
+        if (n !== 1) return x *= pow(x, n - 1);
+        return x;
+    };
+    return callback(a, b, pow(a, b));
+}
 
-// console.log(myPow(2, 3, myPrint)); // 2^3=8
+console.log(myPow(3, 4, myPrint)); // 3^4=81
 
-/*
- * #4
- *
- * Создайте несколько однотипных объектов для описания автомобиля. Соблюдайте следующие правила, используйте следующие поля:
- * имя объекта car – обязательно и необходимое для тестирования, дальнейшее именование объектов – на ваше усмотрение
- * car.engine – объем двигателя, числовое поле
- * car.model – модель авто, строка
- * car.name – бренд авто, строка
- * car.year – год выпуска, число
- * car.used – строка для описания состояния авто, допускаются значения 'used' и 'new'
- *
- * #5
- *
- * Для созданных ранее объектов определите метод info(), используя ключевое слово this.
- * данный метод должен формировать и возвращать строку с полной информацией об автомобиле, например:
- * Chevrolet Lacetti, 2000cc, year 2010, used
- * Infinite FX50 AWD, 5000cc, year 2019, new
- * пробелы, запятые, символы cc и текст – имеют значение и проверяются при тестировании кода
- *
- * #6
- *
- * Для созданных ранее объектов измените свойство used, используя аксессоры (геттер и сеттер).
- * - используйте текущий год либо непосредственно в своем коде, либо с помощью глобальной переменной, например, yearNow
- * - если год выпуска автомобиля отличается от текущего года, геттер used должен выводить текст 'used'
- * - если год выпуска автомобиля совпадает с текущим годом, геттер used должен выводить текст 'new'
- * - если сеттеру used присвоено значение 'new', при этом года выпуска автомобиля отличается от текущего года,
- * - необходимо изменить год выпуска автомобиля, установив в качестве значения текущий год
- * - если сеттеру used присвоено значение 'used', ничего делать не нужно
- */
+console.log(myPow(2, 3, myPrint)); // 2^3=8
 
-// let yearNow = new Date().getFullYear(); // получить текущий год как число
 
-// console.log(car.info()); // Chevrolet Lacetti, 2000cc, year 2010, used
 
-// car.used = 'new';
+console.log('---------------')
 
-// console.log(car.info()); // Chevrolet Lacetti, 2000cc, year 2019, new -- год изменен
 
-// car.used = 'used';
 
-// console.log(car.info()); // Chevrolet Lacetti, 2000cc, year 2019, new -- изменения не выполняются
 
-// console.log(car2.info()); // Infinite FX50 AWD, 5000cc, year 2019, new
+//  * #4
 
-// car.used = 'used';
+//  *
+//  * Создайте несколько однотипных объектов для описания автомобиля. Соблюдайте следующие правила, используйте следующие поля:
+//  * имя объекта car – обязательно и необходимое для тестирования, дальнейшее именование объектов – на ваше усмотрение
+//  * car.engine – объем двигателя, числовое поле
+//  * car.model – модель авто, строка
+//  * car.name – бренд авто, строка
+//  * car.year – год выпуска, число
+//  * car.used – строка для описания состояния авто, допускаются значения 'used' и 'new'
+//  * 
+//  * #5
+//  *
+//  * Для созданных ранее объектов определите метод info(), используя ключевое слово this.
+//  * данный метод должен формировать и возвращать строку с полной информацией об автомобиле, например:
+//  * Chevrolet Lacetti, 2000cc, year 2010, used
+//  * Infinite FX50 AWD, 5000cc, year 2019, new
+//  * пробелы, запятые, символы cc и текст – имеют значение и проверяются при тестировании кода
+//  *
+//  * #6
+//  *
+//  * Для созданных ранее объектов измените свойство used, используя аксессоры (геттер и сеттер).
+//  * - используйте текущий год либо непосредственно в своем коде, либо с помощью глобальной переменной, например, yearNow
+//  * - если год выпуска автомобиля отличается от текущего года, геттер used должен выводить текст 'used'
+//  * - если год выпуска автомобиля совпадает с текущим годом, геттер used должен выводить текст 'new'
+//  * - если сеттеру used присвоено значение 'new', при этом года выпуска автомобиля отличается от текущего года,
+//  * - необходимо изменить год выпуска автомобиля, установив в качестве значения текущий год
+//  * - если сеттеру used присвоено значение 'used', ничего делать не нужно
 
-// console.log(car2.info()); // Infinite FX50 AWD, 5000cc, year 2019, new -- изменения не выполняются
+function fullInfo() {
+    return `${this.name} ${this.model}, ${this.engine}cc, year ${this.year}, ${this.used}`;
+}
+
+let yearNow = new Date().getFullYear(); // получить текущий год как число
+let car = {
+    engine: 2000,
+    model: 'Lacetti',
+    name: 'Chevrolet',
+    year: 2010,
+    info: fullInfo,
+    get used() {
+        return this.year !== yearNow ? 'used' : 'new';
+    },
+    set used(value) {
+        if (value === 'new' && this.year < yearNow) this.year = yearNow;
+    }
+};
+let car2 = {
+    engine: 5000,
+    model: 'FX50 AWD',
+    name: 'Infinite',
+    year: 2019,
+    info: fullInfo,
+    get used() {
+        return this.year !== yearNow ? 'used' : 'new';
+    },
+    set used(value) {
+        if (value === 'new' && this.year < yearNow) this.year = yearNow;
+    }
+};
+
+
+console.log(car.info()); // Chevrolet Lacetti, 2000cc, year 2010, used
+
+car.used = 'new';
+
+console.log(car.info()); // Chevrolet Lacetti, 2000cc, year 2019, new -- год изменен
+
+car.used = 'used';
+
+console.log(car.info()); // Chevrolet Lacetti, 2000cc, year 2019, new -- изменения не выполняются
+
+console.log(car2.info()); // Infinite FX50 AWD, 5000cc, year 2019, new
+
+car.used = 'used';
+
+console.log(car2.info()); // Infinite FX50 AWD, 5000cc, year 2019, new -- изменения не выполняются
+
+
+
+console.log('---------------')
 
 /*
  * #7
@@ -135,9 +224,15 @@ console.log('Sample JavaScript #3 HW #15');
  * В реализации функции должен быть применен метод Math.max() и apply().
  */
 
-// let list = [12, 23, 100, 34, 56, 9, 233];
+let list = [12, 23, 100, 34, 56, 9, 233];
 
-// console.log(myMax(list)); // 233
+let myMax = (arg) => Math.max.apply(Math, arg);
+
+
+console.log(myMax(list)); // 233
+
+
+console.log('---------------')
 
 /*
  * #8
@@ -145,25 +240,40 @@ console.log('Sample JavaScript #3 HW #15');
  * Создайте функцию myMul(a, b), которая будет умножать числа а и b, возвращая результат.
  */
 
+function myMul(a, b) {
+    return a * b;
+}
+
 /*
  * создайте функции myDouble(n), которая принимает один параметр и  удваивает его.
  * Использовать умножение или другие математические операции внутри функции – запрещено, только bind() и myMul().
  * Функция возвращает результат вычисления.
  */
 
-// console.log(myDouble(3)); // = myMul(2, 3) = 6
 
-// console.log(myDouble(4)); // = myMul(2, 4) = 8
+let myDouble = myMul.bind(null, 2)
 
-// console.log(myDouble(5)); // = myMul(2, 5) = 10
+console.log(myDouble(3)); // = myMul(2, 3) = 6
+
+console.log(myDouble(4)); // = myMul(2, 4) = 8
+
+console.log(myDouble(5)); // = myMul(2, 5) = 10
 
 // аналогичным образом создайте функцию myTriple(n), которая утраивает принимающий параметр, возвращая результат.
 
-// console.log(myTriple(3)); // = myMul(3, 3) = 9
 
-// console.log(myTriple(4)); // = myMul(3, 4) = 12
+let myTriple = myMul.bind(null, 3)
 
-// console.log(myTriple(5)); // = myMul(3, 5) = 15
+console.log(myTriple(3)); // = myMul(3, 3) = 9
+
+console.log(myTriple(4)); // = myMul(3, 4) = 12
+
+console.log(myTriple(5)); // = myMul(3, 5) = 15
+
+
+
+console.log('---------------')
+
 
 /*
  * #9
@@ -175,10 +285,19 @@ console.log('Sample JavaScript #3 HW #15');
  * Любые условные операторы – запрещены и объекты.
  */
 
-// let notUniqNums = [1, 1, 2, 3, 4, 5, 6, 7];
 
-// let notUniqStrings = ['Bob', 'Kate', 'Jhon', 'Tom', 'Jhon', 'Kate', 'Tom', 'Bob', 'Jhon', 'Tom'];
+let notUniqNums = [1, 1, 2, 3, 4, 5, 6, 7];
 
-// console.log(myUniq(notUniqNums));
+let notUniqStrings = ['Bob', 'Kate', 'Jhon', 'Tom', 'Jhon', 'Kate', 'Tom', 'Bob', 'Jhon', 'Tom'];
 
-// console.log(myUniq(notUniqStrings));
+let myUniq = (arr) => {
+    let set = new Set();
+        arr.forEach((val) => {
+            set.add(val);
+        });
+    return set;
+};
+
+console.log(myUniq(notUniqNums));
+
+console.log(myUniq(notUniqStrings));
